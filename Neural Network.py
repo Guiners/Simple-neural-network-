e4441d6c35e9f5d4d6df1bc75212be819b98d494
Guiners <50328216+Guiners@users.noreply.github.com>
2020-07-08 20:37:47 +0200

    Final form

--- Neural Network.py
+++ Neural Network.py
@@ -1,81 +1,79 @@
 import numpy as np
-#data
-start_inputs = np.array([[0, 0, 1], [1, 1, 1], [0, 1, 1], [1, 0, 1]])
-#print(start_inputs)
-start_outputs = np.array([0, 0, 1, 1])
-#print(start_outputs)
-start_outputs = start_outputs.reshape(4, 1)
-#print(mixed_start_outputs)
-#np.random.seed()
+import pandas as pd
 
-def generate_weights(inputs):
-    while True:
-        weights = np.random.randint(-10.0, 10.0, (len(inputs) - 1))
-        weights = weights * 0.11111111111111 #wait
-        if weights.mean() == 0:
-            #print(weights)
-            break
-    weights = weights.reshape(3, 1)
-    return weights
+def generate_weight(x,y):
+    return 2 * np.random.random((x, y)) - 1
 
-def output_calc(input, weight, bias,):
-    output = np.dot(input, weight) + bias
-    return output
+def sigmoid_calc(x):
+    return 1/(1 + np.exp(-x))
 
-def sigmoid_calc(input):
-    sigmoid = 1/(1 + np.exp(-input))
-    return sigmoid
+def sigmoid_der_calc(x):
+    return x * (1 - x)
 
-def sigmoid_der_calc(input):
-    sigmoid_der = sigmoid_calc(input) * (1 - sigmoid_calc(input))
-    return sigmoid_der
-
-def test(point):
-    result = sigmoid_calc(output_calc(point, weights, bias))
-    if result > 0.5:
-        output = 1
+def test(x):
+    result = sigmoid_calc(np.dot(x, weight0))
+    result = sigmoid_calc(np.dot(result, weight1))
+    if result > 0.4:
+        value = 1
     else:
-        output = 0
-    print("network thinks that is", output, "with", result, "probability")
-    return result, output
-#parameters
-bias = 3
-learning_rate = 0.05
-weights = generate_weights(start_inputs)
-#print(weights)
+        value = 0
 
-for i in range(50001):
-    inputs = start_inputs
+    return result, value
 
-    #feedforward
-    xw = output_calc(inputs, weights, bias) #change name
-    z = sigmoid_calc(xw)    #change name
-    #print(z)
 
-    #backpropagation
-    error = z - start_outputs
-    error_sum = error.sum()
-    #print(error_sum)
-    dcost_dpred = error #change name
-    dpred_dz = sigmoid_der_calc(z) #change name
 
-    #slope = inputs * dcost_dpred * dpred_dz
-    z_delta = dcost_dpred * dpred_dz #change name
+#data
+inputs = np.array([[0, 0, 1],
+                    [1, 1, 1],
+                    [0, 1, 1],
+                    [1, 0, 1]])
 
-    inputs = start_inputs.transpose()
-    #print('a', np.dot(inputs, z_delta))
-    #print('b', np.dot(inputs, z_delta)* learning_rate)
-    #print('c', weights)
-    #print(type(np.dot(inputs, z_delta)))
-    weights = weights - (learning_rate * np.dot(inputs, z_delta)) 
-    #print(weights)
-    for x in z_delta:
-        bias -= learning_rate * x
+outputs = np.array([[0],
+                    [0],
+                    [1],
+                    [1]])
 
-    if i%10000 == 0:
-        print("value of error", error_sum)
+#seed
+np.random.seed(23)
 
-#test
-point = np.random.randint(0, 2, 3)
-print(point)
-a, b = test(point)
+#weights
+weight0 = generate_weight(3, 4)
+weight1 = generate_weight(4, 1)
+
+for epoch in range(50000):
+
+    layer0 = inputs           # input layer
+
+    layer1 = np.dot(layer0, weight0)    # first layer
+    layer1 = sigmoid_calc(layer1)
+
+    layer2 = np.dot(layer1, weight1)    # second layer
+    layer2 = sigmoid_calc(layer2)
+
+    layer2_error = outputs - layer2    #comparing output from data with prediction
+
+    if epoch%10000 == 0:
+        error = np.mean(np.abs(layer2_error))
+        print("error value:" + str(error))
+
+    layer2_delta = layer2_error * sigmoid_der_calc(layer2)  #getting info to change weights
+
+    layer1_error = layer2_delta.dot(weight1.T)
+    layer1_delta = layer1_error * sigmoid_der_calc(layer1)   #getting info to change weights
+
+    weight1 += layer1.T.dot(layer2_delta)                     #improving weights
+    weight0 += layer0.T.dot(layer1_delta)
+
+
+
+values = []
+predictions = []
+
+for i in inputs:            #getting info for data frame
+    res, val = test(i)
+    values.append(val)
+    predictions.append(res)
+
+data = {'Inputs': inputs.tolist(), 'Output': outputs.tolist(),'Network prediction': predictions, 'Network output': values}      #creating dataframe
+df = pd.DataFrame(data=data)
+print(df)
